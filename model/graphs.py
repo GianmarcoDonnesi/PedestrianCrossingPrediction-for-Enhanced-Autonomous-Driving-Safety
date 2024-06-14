e018ff565cae39cb8d38c120bfcdd8aaed26accf
Gianmarco Donnesi <gianmarco.donnesi@outlook.it>
2024-06-14 17:11:18 +0200

    update graphs.py

--- model/graphs.py
+++ model/graphs.py
@@ -10,12 +10,21 @@ with open('./results.pkl', 'rb') as f:
 # Convert results to DataFrame
 results_df = pd.DataFrame(results)
 
+# Define a custom color palette
+custom_palette = {
+    'None': '#1f77b4',  # blue
+    'Traffic': '#2ca02c',  # green
+    'Vehicle': '#d62728',  # red
+    'Appearance': '#ffdf00',  # yellow
+    'Attributes': '#ff7f0e'  # orange
+}
+
 # Set up the matplotlib figure
 plt.figure(figsize = (15, 5))
 
 # Plot Accuracy
 plt.subplot(1, 3, 1)
-sns.barplot(data=results_df, x = 'ablation', y = 'accuracy')
+sns.barplot(data=results_df, x = 'ablation', y = 'accuracy', palette=custom_palette, width=0.6)
 plt.title('Validation Accuracy')
 plt.ylim(0, 1)
 plt.ylabel('Accuracy')
@@ -23,7 +32,7 @@ plt.xlabel('Ablation')
 
 # Plot Recall
 plt.subplot(1, 3, 2)
-sns.barplot(data = results_df, x = 'ablation', y = 'recall')
+sns.barplot(data = results_df, x = 'ablation', y = 'recall', palette=custom_palette, width=0.6)
 plt.title('Validation Recall')
 plt.ylim(0, 1)
 plt.ylabel('Recall')
@@ -31,7 +40,7 @@ plt.xlabel('Ablation')
 
 # Plot F1 Score
 plt.subplot(1, 3, 3)
-sns.barplot(data = results_df, x = 'ablation', y = 'f1_score')
+sns.barplot(data = results_df, x = 'ablation', y = 'f1_score', palette=custom_palette, width=0.6)
 plt.title('Validation F1 Score')
 plt.ylim(0, 1)
 plt.ylabel('F1 Score')
